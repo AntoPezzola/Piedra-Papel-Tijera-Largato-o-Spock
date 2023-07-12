4852591f7d5dc63ae0fdb61ff4d5844178e16739
Anto Pezzola <antopezzola@gmail.com>
2023-07-12 02:22:51 -0300

    Resultado y puntajes de jugador agregados

--- src/components/ScreenJuego.js
+++ src/components/ScreenJuego.js
@@ -1,159 +1,79 @@
-import React, { useEffect, useState } from "react";
-import './ScreenJuego.css';
-import { useTrail } from "react-spring";
+import React, { useEffect , useState } from "react";
+import "./ScreenJuego.css";
 import Opciones from "./Opciones";
-import { toast } from "react-toastify";
+import Animacion from "./Animacion";
+import opcionesJuego from "./OpcionesLista";
+import useJuego from "./UsosDelJuego";
 
-const piedra = "https://img.icons8.com/fluency/70/rock.png";
-const papel = "https://img.icons8.com/external-tulpahn-flat-tulpahn/70/external-paper-stationery-tulpahn-flat-tulpahn.png";
-const tijeras = "https://img.icons8.com/fluency/70/cut.png";
-const lagarto = "https://img.icons8.com/color/70/crocodile-icon.png";
-const spock = "https://img.icons8.com/color/70/star-trek-gesture.png";
-const iconoVolver = "https://img.icons8.com/ios/80/return.png";
-
-
-const opciones = [
-  { id: 0, nombre: "Piedra", imagen: piedra, vence: [2, 3] },
-  { id: 1, nombre: "Papel", imagen: papel, vence: [0] },
-  { id: 2, nombre: "Tijera", imagen: tijeras, vence: [1, 3] },
-  { id: 3, nombre: "Lagarto", imagen: lagarto, vence: [1] },
-  { id: 4, nombre: "Spock", imagen: spock, vence: [3, 0] },
-];
-
+const iconoVolver = "https://img.icons8.com/ios/70/return.png";
 
 const ScreenJuego = ({ volverInicio }) => {
-  const [eleccionUser, setEleccionUser] = useState(null);
-  const [eleccionPc, setEleccionPc] = useState(null);
-  const [resultado, setResultado] = useState(null);
-  const [pcMessage, setPcMessage] = useState(null);
-  const [userMessage, setUserMessage] = useState(null);
-  const [showAnimation, setShowAnimation] = useState(false);
-  const [contadorUser, setContadorUser] = useState(0);
-  const [contadorOponente, setContadorOponente] =  useState(0);  
+  const {
+    eleccionUsuario,
+    eleccionOponente,
+    resultado,
+    contadorUsuario,
+    contadorOponente,
+    handleClickOption,
+    reset,
+  } = useJuego(opcionesJuego);
 
+  const [userMessage, setUserMessage] = useState("");
+  const [pcMessage, setPcMessage] = useState("");
 
-
-  const obtenerResultado = (eleccionUser, eleccionPc) => {
-    if (eleccionUser === eleccionPc) {
-      return 0;
-    }
-    if (opciones[eleccionUser]?.vence.includes(eleccionPc)) {
-      setContadorUser(contadorUser + 1); 
-      return 1;
-    }
-    setContadorOponente(contadorOponente + 1); 
-    return 2;
-  };
-
-
-  const reset = () => {
-    setEleccionUser(null);
-    setEleccionPc(null);
-    setUserMessage(null);
-    setPcMessage(null);
-    setShowAnimation(true);
-    setResultado(null);
-    resetContador(); 
-  };
-
-  const resetContador = () => {
-    if (contadorOponente === 3 || contadorUser === 3) {
-      if (contadorOponente === 3) {
-        toast.error('Perdiste la Partida!', {
-        position: "bottom-center"
-      }) 
-      } else {
-        toast.success("Ganaste la partida!", {
-          position: "bottom-center"
-        })
-      }
-      setContadorOponente(0);
-      setContadorUser(0);
-    }
-  }
-
-  const handleClickOption = (eleccion) => {
-    setEleccionUser(eleccion);
-    setShowAnimation(false)
-
-    const numeroRandom = Math.floor(Math.random() * 5);
-
-    setTimeout(() => {
-      setEleccionPc(numeroRandom);
-    }, 1500);
-
-    setTimeout(() => {
-      setResultado(obtenerResultado(eleccion, numeroRandom));
-    }, 3000);
-
-    clearTimeout();
-  };
 
   useEffect(() => {
-    if (eleccionUser !== null) {
-      setUserMessage(`Elegiste ${opciones[eleccionUser]?.nombre}`);
+    if (eleccionUsuario !== null) {
+      setUserMessage(`Elegiste ${opcionesJuego[eleccionUsuario]?.nombre}`);
     }
-  }, [eleccionUser]);
+  }, [eleccionUsuario]);
 
   useEffect(() => {
-    if (eleccionPc !== null) {
-      setPcMessage(`El oponente eligio ${opciones[eleccionPc]?.nombre}`);
+    if (eleccionOponente !== null) {
+      setPcMessage(`El oponente eligió ${opcionesJuego[eleccionOponente]?.nombre}`);
     }
-  }, [eleccionPc]);
+  }, [eleccionOponente]);
 
 
-  
-  useEffect(() => {
-    setShowAnimation(true);
-  }, []);
-
-
-  const buttonsAnimation = useTrail(opciones.length, {
-    opacity: showAnimation ? 1 : 0,
-    transform: showAnimation ? "translateY(0)" : "translateY(-20px)",
-    config: { tension: 200, friction: 20 },
-  });
+  const buttonsAnimation = Animacion(opcionesJuego, eleccionUsuario === null);
 
   return (
     <div className="Juego-Comenzado">
       <button className="volverBoton" onClick={volverInicio}>
         <img src={iconoVolver} alt="Volver" />
       </button>
-      <h1 className="elegir-Opcion">Elige una opción!</h1>
+      {eleccionUsuario === null && <h1 className="elegir-Opcion">¡Elige una opción!</h1>}
       <div className="resultado-juego">
         <div className="contador">
-           <p className="contador-User"> Tus puntos: {contadorUser} </p>
-           <p className="contador-Oponente"> Puntos Oponente: {contadorOponente} </p>
+          <p className="contador-Usuario">Puntos Jugador: {contadorUsuario}</p>
+          <p className="contador-Oponente">Puntos Oponente: {contadorOponente}</p>
         </div>
-        <div className="opciones">
-            <Opciones
-            opciones={opciones}
-            handleClickOption={handleClickOption}
-            buttonsAnimation={buttonsAnimation}
-          />
-        </div>
-        <div className="resultado">
-          {eleccionUser !== null && <p>{userMessage}</p>}
-          {eleccionPc !== null && <p>{pcMessage}</p>}
-          {resultado === 0 && <p>Hay un empate!</p>}
-          {resultado === 1 && (
-            <p>
-              ✅ {opciones[eleccionUser]?.nombre} le gana a {" "}
-              {opciones[eleccionPc]?.nombre}
-            </p>
-          )}
-          {resultado === 2 && (
-            <p>
-              ❌ {opciones[eleccionUser]?.nombre} pierde contra {" "}
-              {opciones[eleccionPc]?.nombre}
-            </p>
-          )}
-          {resultado !== null && (
-            <button className="boton-volver-a-jugar" onClick={reset}>
-              <p> Volver a jugar! </p>
-            </button>
-          )}
-        </div>
+        {eleccionUsuario === null ? (
+          <div className="opciones">
+            <Opciones opciones={opcionesJuego} handleClickOption={handleClickOption} buttonsAnimation={buttonsAnimation} />
+          </div>
+        ) :  (
+          <div className="resultado">
+            {eleccionUsuario !== null && <p>{userMessage}</p>}
+            {eleccionOponente !== null && <p>{pcMessage}</p>}
+            {resultado === 0 && <p className="resultado-empate">¡Ninguno se lleva el punto!</p>}
+            {resultado === 1 && (
+              <p className="resultado-ganador">
+                ✅ {opcionesJuego[eleccionUsuario]?.nombre} le gana a {opcionesJuego[eleccionOponente]?.nombre}, ¡ganaste un punto!
+              </p>
+            )}
+            {resultado === 2 && (
+              <p className="resultado-perdedor">
+                ❌ {opcionesJuego[eleccionUsuario]?.nombre} pierde contra {opcionesJuego[eleccionOponente]?.nombre}, ¡el oponente gana un punto!
+              </p>
+            )}
+            {resultado !== null && (
+              <button className="boton-volver-a-jugar" onClick={reset}>
+                Volver a jugar
+              </button>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/components/UsosDelJuego.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const useJuego = (opcionesJuego) => {
+  const [eleccionUsuario, setEleccionUsuario] = useState(null);
+  const [eleccionOponente, setEleccionOponente] = useState(null);
+  const [resultado, setResultado] = useState(null);
+  const [contadorUsuario, setContadorUsuario] = useState(0);
+  const [contadorOponente, setContadorOponente] = useState(0);
+
+
+  const obtenerResultado = (eleccionUsuario, eleccionOponente) => {
+    if (eleccionUsuario === eleccionOponente) {
+      return 0;
+    }
+    if (opcionesJuego[eleccionUsuario]?.vence.includes(eleccionOponente)) {
+      setContadorUsuario(contadorUsuario + 1);
+      return 1;
+    }
+    setContadorOponente(contadorOponente + 1);
+    return 2;
+  };
+
+  const reset = () => {
+    setEleccionUsuario(null);
+    setEleccionOponente(null);
+    setResultado(null);
+    resetContador();
+  };
+
+  const resetContador = () => {
+    if (contadorOponente === 3 || contadorUsuario === 3) {
+      if (contadorOponente === 3) {
+        toast.error("¡Perdiste la Partida!", {
+          position: "bottom-center",
+        });
+      } else {
+        toast.success("¡Ganaste la partida!", {
+          position: "bottom-center",
+        });
+      }
+      setContadorOponente(0);
+      setContadorUsuario(0);
+    }
+  };
+
+  const handleClickOption = (eleccion) => {
+    setEleccionUsuario(eleccion);
+
+    const numeroRandom = Math.floor(Math.random() * opcionesJuego.length);
+
+    setTimeout(() => {
+        setEleccionOponente(numeroRandom);
+      }, 1500);
+
+    setTimeout(() => {
+      setResultado(obtenerResultado(eleccion, numeroRandom));
+    }, 3000);
+  };
+
+  return {
+    eleccionUsuario,
+    eleccionOponente,
+    resultado,
+    contadorUsuario,
+    contadorOponente,
+    handleClickOption,
+    reset,
+  };
+};
+
+export default useJuego;
